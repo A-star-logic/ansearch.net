26c2c0bb38f5e051f9e293f68efca260bb58251b
Guillaume <guillaume@grid-agency.com>
2025-12-02 19:10:10 +0100

    Added articles filtering

--- src/content.config.ts
+++ src/content.config.ts
@@ -87,8 +87,24 @@ const products = defineCollection({
   type: 'content',
 });
 
+const blog = defineCollection({
+  type: 'content',
+  schema: z.object({
+    layout: z.string(),
+    title: z.string(),
+    author: z.string(),
+    categories: z.array(z.string()).default([]),
+    tags: z.array(z.string()).default([]),
+    image: z.string().optional(),
+    imageAlt: z.string().optional(),
+    date: z.string(),
+    edited: z.string().optional(),
+  }),
+});
+
 export const collections = {
   'landing-pages': landingPages,
   policies: policies,
   products: products,
+  blog: blog,
 };
